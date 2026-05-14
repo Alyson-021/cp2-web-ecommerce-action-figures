a5db47649c60036b79d9197930caecbba7d8bb91
Alyson Souto <alysonartist@gmail.com>
2026-05-14 20:45:44 -0300

    feat: criação do array de produtos

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+//Array é uma estrutura usada para guardar vários valores dentro de uma única variável.
+// produtos = [
+//    {produto1},
+//    {produto2},
+//    produto3,
+//    produto4,
+//    produto5
+// ]
+
+
+const produtos = [
+
+    {
+        nome: "Dante DMC 5",
+        preco: 299.99,
+        imagem: "./assets/img/dante.jpeg"
+    },
+
+    {
+        nome: "Ellie TLOU 2",
+        preco: 349.90,
+        imagem: "./assets/img/ellie.jpeg"
+    },
+
+    {
+        nome: "Kratos GOW",
+        preco: 399.90,
+        imagem: "./assets/img/kratos.jpeg"
+    },
+
+    {
+        nome: "Leon RE 4",
+        preco: 279.90,
+        imagem: "./assets/img/leon.jpeg"
+    },
+
+    {
+        nome: "Malenia Elden Ring",
+        preco: 459.90,
+        imagem: "./assets/img/malenia.jpeg"
+    }
+
+];
